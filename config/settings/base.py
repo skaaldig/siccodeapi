6ea6841f79cc605799e2c302d8460ced332946e1
Skaaldig <skaaldig@gmail.com>
2018-11-06 10:53:51 -0500

    Throttling added

--- config/settings/base.py
+++ config/settings/base.py
@@ -240,5 +240,15 @@ SOCIALACCOUNT_ADAPTER = 'sicem.users.adapters.SocialAccountAdapter'
 # Your stuff...
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '150/day',
+        'user': '3000/day'
+    }
 }
