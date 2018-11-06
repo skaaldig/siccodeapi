a4f36c3ca3c67fbc1ba498a9ca117b6c994d79db
Skaaldig <skaaldig@gmail.com>
2018-11-06 10:35:37 -0500

    changed views to read only since sic codes are static

--- siccodes/views.py
+++ siccodes/views.py
@@ -3,25 +3,25 @@ from . import models
 from . import serializers
 
 
-class IndustrySectorViewSet(viewsets.ModelViewSet):
+class IndustrySectorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.IndustrySector.objects.all()
     serializer_class = serializers.IndustrySectorSerializer
-    filter_fields = ('sector_id',)
+    filter_fields = ('sector_id', 'description')
 
 
-class IndustryGroupViewSet(viewsets.ModelViewSet):
+class IndustryGroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.IndustryGroup.objects.all()
     serializer_class = serializers.IndustryGroupSerializer
-    filter_fields = ('group_id', 'sector_id')
+    filter_fields = ('group_id', 'sector_id', 'description')
 
 
-class IndustryViewSet(viewsets.ModelViewSet):
+class IndustryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Industry.objects.all()
     serializer_class = serializers.IndustrySerializer
-    filter_fields = ('industry_id', 'group_id')
+    filter_fields = ('industry_id', 'group_id', 'description')
 
 
-class SicCodeViewSet(viewsets.ModelViewSet):
+class SicCodeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.SicCode.objects.all()
     serializer_class = serializers.SicCodeSerializer
-    filter_fields = ('sic_code',)
+    filter_fields = ('sic_code', 'description')
